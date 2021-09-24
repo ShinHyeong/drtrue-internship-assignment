5b36b9f4dece121a06cb47c65c06b1b3e85982e1
ShinHyeong <limo730@naver.com>
2021-09-24 13:20:48 +0900

    미션3 완료

--- src/SiteInfo.js
+++ src/SiteInfo.js
@@ -9,26 +9,26 @@ function SiteInfo() {
     try {
       // loading 상태를 true로 초기화
       setLoading(true);
-      const response = await axios.get(
-        "https://dapi.kakao.com/v2/search/image?query=안녕",
+      const response = await axios.get( //await를 사용하여 비동기적으로 Rest API를 GET방식으로 호출
+        "https://dapi.kakao.com/v2/search/image?query=안녕", //target Rest API
         {
           headers: {
             Authorization: `KakaoAK facbb84a986a578369c2938173b5a8ae`,
           },
         }
       );
-      setResult(response.data.documents);
+      setResult(response.data.documents); //결과 가져오기
     } catch (e) {
     }
-    setLoading(false);
+    setLoading(false); //loading 끝
   };
 
   useEffect(() => {
     fetchInfos();
   }, []);
   if (loading) return <div>로딩중..</div>;
-return (
-    <table class="content-table">
+  return ( //화면에서 table형식으로 나타내기
+    <table class="content-table"> 
       <thead>
         <tr>
           <th>Collection</th>
