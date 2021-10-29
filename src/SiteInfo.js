950c268f4a708a49f8b6fdd835883ebdca346bda
ShinHyeong <limo730@naver.com>
2021-10-29 16:42:28 +0900

    미션5 완료

--- src/SiteInfo.js
+++ src/SiteInfo.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Table from "./components/Table";
+import Pagination from "./components/Pagination";
+import axios from "axios";
+
 function SiteInfo() {
+  const [Post, setPost] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [result, setResult] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
 
   const fetchInfos = async () => {
     try {
-      // loading 상태를 true로 초기화
-      setLoading(true);
+      setLoading(true);// loading 상태를 true로 초기화
       const response = await axios.get( //await를 사용하여 비동기적으로 Rest API를 GET방식으로 호출
         "https://dapi.kakao.com/v2/search/image?query=안녕", //target Rest API
         {
@@ -17,7 +20,7 @@ function SiteInfo() {
           },
         }
       );
-      setResult(response.data.documents); //결과 가져오기
+      setPost(response.data.documents); //결과 가져오기
     } catch (e) {
     }
     setLoading(false); //loading 끝
@@ -26,10 +29,27 @@ function SiteInfo() {
   useEffect(() => {
     fetchInfos();
   }, []);
+
   if (loading) return <div>로딩중..</div>;
-  return ( 
-    <Table /> //화면에서 table형식으로 나타내기
-  );
-}
+
+    // Get current posts
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = Post.slice(indexOfFirstPost, indexOfLastPost);
+
+    // Change page
+    const paginate = pageNumber => setCurrentPage(pageNumber);
+
+    return (
+      <div>
+      <Table table_data={currentPosts}/>
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={Post.length}
+        paginate={paginate}
+      />
+      </div>
+      );
+    };
 
 export default SiteInfo;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,52 +1,35 @@
-import React, { Component } from 'react';
-import {result} from '../SiteInfo';
-import PropTypes from 'prop-types';
-
-class Table extends Component {
-  render() {
-    const {result} = this.props;
-    return (
-      <table class="content-table"> 
+import React from "react";
+import './Table.css';
+function Table({ table_data }) {
+  return (
+      <table className="content-table">
         <thead>
           <tr>
-            <th>Collection</th>
-            <th>Datetime</th>
-            <th>Site Name</th>
-            <th>Document Url</th>
-            <th>Height</th>
-            <th>Image Url</th>
-            <th>Thumbnail Url</th>
-            <th>Width</th>
+            <th>collection</th>
+            <th>datetime</th>
+            <th>sitename</th>
+            <th>url</th>
+            <th>height</th>
+            <th>image_url</th>
+            <th>thumbnail_url</th>
+            <th>width</th>
           </tr>
         </thead>
-        <tbody>
-          {result.map((document, index) => (
-              <tr key={index}>
-                <td>{document.collection}</td>             
-                <td>{document.datetime}</td>
-                <td>{document.display_sitename}</td>
-                <td>{document.doc_url}</td>
-                <td>{document.height}</td>
-                <td>{document.image_url}</td>
-                <td>{document.thumbnail_url}</td>
-                <td>{document.width}</td>
-              </tr>
-              )
-          )}
+      <tbody>
+        {table_data.map((document, index) => (
+          <tr key={index}>
+            <td>{document.collection}</td>
+            <td>{document.datetime}</td>
+            <td>{document.display_sitename}</td>
+            <td>{document.doc_url}</td>
+            <td>{document.height}</td>
+            <td>{document.image_url}</td>
+            <td>{document.thumbnail_url}</td>
+            <td>{document.width}</td>
+          </tr>
+          ))}
         </tbody>
-      </table>
-    );
-  }
+      </table>        
+  );
 }
-
-Table.propTypes = { 
-  collection: PropTypes.string.isRequired,
-  datetime: PropTypes.string.isRequired,
-  display_sitename: PropTypes.string.isRequired,
-  doc_url: PropTypes.string.isRequired,
-  height: PropTypes.number.isRequired,
-  image_url: PropTypes.string.isRequired,
-  thumbnail_url: PropTypes.string.isRequired,
-  width: PropTypes.number.isRequired
-};
 export default Table;
